770e4070e95357490e43dd0551dbdb32c867f927
Pablo Esteban Olate Carreno <47009066+krockzs@users.noreply.github.com>
2026-09-10 18:41:09 -0300

    Connect installer controller to QML

--- ui/installer/main.cpp
+++ ui/installer/main.cpp
@@ -1,11 +1,19 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
+#include <QQmlContext>
+
+#include "installercontroller.h"
 
 int main(int argc, char *argv[])
 {
     QGuiApplication app(argc, argv);
+    app.setApplicationName(QStringLiteral("N.E.E.B.L.E.S. Installer"));
+    app.setOrganizationName(QStringLiteral("N.E.E.B.L.E.S."));
+
+    InstallerController installer(app.arguments());
 
     QQmlApplicationEngine engine;
+    engine.rootContext()->setContextProperty(QStringLiteral("installer"), &installer);
     engine.loadFromModule("NeeblesInstaller", "Main");
 
     if (engine.rootObjects().isEmpty())
