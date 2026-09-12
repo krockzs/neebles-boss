2aefddad48c5c53f200c4739a45384703c6ea003
Olate Stark y Jarvis <47009066+krockzs@users.noreply.github.com>
2026-09-12 04:06:28 -0300

    Fix Boss version reporting for 1.0.2

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ mod notifications;
 mod privileges;
 mod request;
 
-pub const VERSION: &str = "1.0.1";
+pub const VERSION: &str = env!("CARGO_PKG_VERSION");
 
 fn main() {
     let args = std::env::args().skip(1).collect::<Vec<_>>();
